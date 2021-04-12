a188f891565ca453762e7f3c73e948e014bd6bfe
kmaasrud <km@aasrud.com>
2021-04-12 14:41:12 +0200

    Cleaned old results and scripts

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ mod wavefunction;
 mod hamiltonian;
 mod montecarlo;
 mod threadpool;
-mod produce_output;
-mod create_output;
 
 pub use particle::Particle;
 pub use system::System;
@@ -18,10 +16,5 @@ pub use threadpool::ThreadPool;
 
 
 fn main() {
-    // produce_output::track_each_cycle();
-    // produce_output::dim_and_n();
-    // produce_output::interacting_elliptical();
-    // produce_output::sgd_noninteracting();
-    // produce_output::sgd_interacting();
-    create_output::metropolis();
+    println!("Hello from VMC!")
 }
