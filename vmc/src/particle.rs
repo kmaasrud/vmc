5af31e3069cfec0cbce2739c12c210724da57ede
kmaasrud <km@aasrud.com>
2021-05-19 10:19:02 +0200

    Vector enum written and almost completely implemented in Particle (#21)

--- vmc/src/particle.rs
+++ vmc/src/particle.rs
@@ -1,7 +1,16 @@
+#[derive(Debug, Clone)]
+enum Vector {
+    D1(f64),
+    D2(f64, f64),
+    D3(f64, f64, f64)
+}
+
+use Vector::*;
+
 /// Struct that represents a single particle.
 #[derive(Debug, Clone)]
 pub struct Particle {
-    pub position: Vec<f64>,
+    pub position: Vector,
     pub qforce: Vec<f64>,
     pub dim: usize,
 }
@@ -9,49 +18,51 @@ pub struct Particle {
 impl Particle {
     /// Creates a new particle with a given dimensionality.
     /// The particle's initial position is set to 0.
-    pub fn new(dim: usize) -> Self {
-        Particle {
-            position: vec![0.; dim],
-            qforce: vec![0.; dim],
-            dim,
-        }
+    pub fn new(dim: usize) -> Result<Self, String> {
+        let position = match dim {
+            1 => D1(0.),
+            2 => D2(0., 0.),   
+            3 => D3(0., 0., 0.),
+            _ => return Err("Unsupported dimensionality.".to_owned()),
+        };
+
+        Ok(Particle{ position, qforce: vec![0.; dim], dim })
     }
 
-    pub fn from_vec(position: Vec<f64>) -> Self {
-        let dim = position.len();
-        Particle {
-            position,
-            qforce: vec![0.; dim],
-            dim,
-        }
+    pub fn from_vector(position: Vector) -> Self {
+        let dim = match position {
+            D1(_) => 1,
+            D2(_,_) => 2,
+            D3(_,_,_) => 3,
+        };
+        Particle { position, qforce: vec![0.; dim], dim, }
     }
 
     /// Computes the squared sum of each coordinate.
     pub fn squared_sum(&self) -> f64 {
-        self.position.iter().map(|x| x.powi(2)).sum()
+        match self.position {
+            D1(x) => x.powi(2),
+            D2(x, y) => x.powi(2) + y.powi(2),
+            D3(x, y, z) => x.powi(2) + y.powi(2) + z.powi(2),
+        }
     }
 
     /// Computes the squared sum of each coordinate, but the z-component is scaled by a factor
     pub fn squared_sum_scaled_z(&self, factor: f64) -> f64 {
-        match self.dim {
-            1 | 2 => self.squared_sum(),
-            _ => {
-                self.position[0].powi(2)
-                    + self.position[1].powi(2)
-                    + factor * self.position[2].powi(2)
-            }
+        match self.position {
+            D1(_) | D2(_,_) => self.squared_sum(),
+            D3(x, y, z) => x.powi(2) + y.powi(2) + factor * z.powi(2),
         }
     }
 
     /// Returns the distance of this particle to other
-    pub fn distance_to(&self, other: &Particle) -> f64 {
-        let result: f64 = other
-            .position
-            .iter()
-            .zip(self.position.iter())
-            .map(|(x, y)| (x - y).powi(2))
-            .sum();
-        result.sqrt()
+    pub fn distance_to(&self, other: &Particle) -> Result<f64, String> {
+        match (self.position, other.position) {
+            (D1(x1), D1(x2)) => Ok((x1 - x2).powi(2).sqrt()),
+            (D2(x1, y1), D2(x2, y2)) => Ok(((x1 - x2).powi(2) + (y1 - y2).powi(2)).sqrt()),
+            (D3(x1, y1, z1), D3(x2, y2, z2)) => Ok(((x1 - x2).powi(2) + (y1 - y2).powi(2) + (z1 - z2).powi(2)).sqrt()),
+            _ => Err("Dimensions do not match".to_owned()),
+        }
     }
 
     /// Adds bump_size to the specified position coordinate
@@ -68,7 +79,7 @@ mod tests {
     fn test_squared_sum_scaled_z() {
         let tol: f64 = 1e-12;
         let want: f64 = 1402.624001;
-        let particle = Particle::from_vec(vec![2.5, 1.001, 68.2]);
+        let particle = Particle::from_vector(D3(2.5, 1.001, 68.2));
         let got: f64 = particle.squared_sum_scaled_z(0.3);
         assert!((want - got).abs() < tol);
     }
@@ -77,15 +88,15 @@ mod tests {
     fn test_distance_to() {
         let tol: f64 = 0.00001;
         let want: f64 = 6.25744;
-        let got: f64 = Particle::from_vec(vec![3., 5.666, 8.])
-            .distance_to(&Particle::from_vec(vec![2., 9., 13.2]));
+        let got: f64 = Particle::from_vector(D3(3., 5.666, 8.))
+            .distance_to(&Particle::from_vector(D3(2., 9., 13.2))).unwrap();
         assert!((want - got).abs() < tol);
     }
 
     #[test]
     fn test_bump_at_dim() {
-        let want = Particle::from_vec(vec![1., 1., 1.6]);
-        let mut got = Particle::from_vec(vec![1., 1., 1.]);
+        let want = Particle::from_vector(D3(1., 1., 1.6));
+        let mut got = Particle::from_vector(D3(1., 1., 1.));
         got.bump_at_dim(2, 0.6);
         assert_eq!(want.position, got.position);
     }
