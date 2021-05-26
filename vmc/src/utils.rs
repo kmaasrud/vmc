7e522d6e9e21e508dfee0ed89f74192e0c755fcf
kmaasrud <km@aasrud.com>
2021-05-26 16:58:06 +0200

    Added new SmallVector type for storing genericly sized arrays

--- vmc/src/utils.rs
+++ vmc/src/utils.rs
@@ -1,3 +1,57 @@
+pub enum SmallVector<T, const N: usize> {
+    Inline(usize, [T; N]),
+    Dynamic(Vec<T>),
+}
+
+impl<T: Copy + Clone, const N: usize> SmallVector<T, N> {
+    fn new(v: T, n: usize) -> Self {
+        if n <= N {
+            Self::Inline(n, [v; N])
+        } else {
+            Self::Dynamic(vec![v; n])
+        }
+    }
+}
+
+impl<T, const N: usize> SmallVector<T, N> {
+    fn iter(&self) -> std::slice::Iter<T> {
+        match self {
+            Self::Inline(_, array) => array.iter(),
+            Self::Dynamic(vec) => vec.iter(),
+        }
+    }
+
+    fn as_slice(&self) -> &[T] {
+        match self {
+            Self::Inline(n, array) => &array[0..*n],
+            Self::Dynamic(vec) => vec,
+        }
+    }
+
+    fn as_mut_slice(&mut self) -> &mut [T] {
+        match self {
+            Self::Inline(n, array) => &mut array[0..*n],
+            Self::Dynamic(vec) => vec,
+        }
+    }
+}
+
+use std::ops::{Deref, DerefMut};
+
+impl<T, const N: usize> Deref for SmallVector<T, N> {
+    type Target = [T];
+
+    fn deref(&self) -> &Self::Target {
+        self.as_slice()
+    }
+}
+
+impl<T, const N: usize> DerefMut for SmallVector<T, N> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.as_mut_slice()
+    }
+}
+
 pub fn det(mat: &Vec<Vec<f64>>) -> Option<f64> {
     // Check for non-square matrix
     if mat.len() != mat[0].len() {
