6e4af0a0cf6e606a3e50d8d3c127da6a0aa7b813
kmaasrud <km@aasrud.com>
2021-06-01 14:45:00 +0200

    Changed header

--- vmc/src/run.rs
+++ vmc/src/run.rs
@@ -35,7 +35,7 @@ pub fn simple() {
         let numerical_str = if numerical_laplace { "numerical" } else { "analytical" };
         path.push(format!("{}_{}_{}.csv", metro_type, interact_str, numerical_str));
         let mut f = create_file(&path);
-        f.write_all("energy[a.u.],time[s]\n".as_bytes()).expect("Unable to write data");
+        f.write_all("energy[au],time[s]\n".as_bytes()).expect("Unable to write data");
 
         // Run 10 times
         for _ in 0..10 {
