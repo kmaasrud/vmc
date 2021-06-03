0363ed314b88f356867024a19249cc88ee7fdfa0
Anna Stray Rongve <54397743+annasro@users.noreply.github.com>
2021-06-03 11:29:30 +0200

    Update OneBodyDensities.py

--- vmc/result_analysis/OneBodyDensities.py
+++ vmc/result_analysis/OneBodyDensities.py
@@ -5,46 +5,35 @@ import matplotlib.pyplot as plt
 
 #making pandas dataframe
 #df = pd.read_csv('../data/filename.csv')
-#position = df["position"].to_list()
-
-size = 1.0
-position  = np.random.rand(1,10)
-position = sorted(position)
-
-step = position[1] - position[0]
-
-max_pos = max(position)
-norm = len(position)
-
-    
-print(position, max_pos)
-
-norm = position[1:]**2/(2*np.pi*position[1:]*position[1] - np.pi*position[1]*position[1])
+#rho2 = df["rho2"].to_list()
+#distance = df["distance"].to_list()
+#max_r  = max(distance)
+#radius = np.random.rand(1,max_r)
 
 
-dr = 0.0
+#dummydata
+max_r = 10
+radius = np.random.rand(1,10)
+rho2 = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
 
-prob = np.zeros(size) 
-counter = 0
-p = 0
+norm = radius[1:]**2/(2*np.pi*radius[1:]*radius[1] - np.pi*radius[1]*radius[1])
 
-while p < norm:
-    if(dr <= position[p] < dr + step):
-        prob[counter] += 1
-        p += 1
-    else:
-        counter += 1
-        dr += step
+with_jastrow    = rho2*norm
+without_jastrow = rho2*norm
 
-norm = len(position)
-prob = prob/(float(norm))
+fig = plt.figure()
 
+plt.plot(radius[1:], with_jastrow/sum(with_jastrow), linestyle = 'None', marker = '-', label = 'With Jastrow factor')
+
+plt.plot(radius[1:], without_jastrow/sum(without_jastrow), linestyle = 'None', marker = '-', label = 'Without Jastrow factor')
 
 
+plt.xlabel(r"$|\mathbf{r}|$", fontsize = 15)
+plt.ylabel(r"$\rho(\mathbf{r})$", fontsize = 15)
+plt.legend(fontsize = 15)
 
-
-
-
+plt.savefig('../plots/onebodydensity_2p.png')
+plt.show()
 
 
 
