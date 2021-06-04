fa4449f5171b9621897bd22830c845070286e34f
Anna Stray Rongve <54397743+annasro@users.noreply.github.com>
2021-06-04 14:34:46 +0200

    bugfixes

--- vmc/result_analysis/SD_energy_min.py
+++ vmc/result_analysis/SD_energy_min.py
@@ -12,7 +12,7 @@ beta    = df['beta'].to_list()
 alpha_woJ = df['alpha_woJ'].to_list() """
 
 #Dummydata: 
-energy = [1,2,3,4,5,6,6,7,-10]
+energy = [1,4,5,6,6.5,7,-10]
 alpha = [0.1, 0.2, 0.3, 0.5, 0, 1, -2]
 alpha_woJ = [0.1, 0.2, 0.3, 0.5, 0, 1, -2]
 beta = alpha 
@@ -22,11 +22,14 @@ energy_min          = min(energy)
 energy_min_index    = energy.index(energy_min) #index of energy_min
 
 #corresponding alpha/beta to energy min
-alpha   = alpha[energy_min_index]
-beta    = beta[energy_min_index]
+alpha       = alpha[energy_min_index]
+alpha_woJ   = alpha_woJ[energy_min_index]
+beta        = beta[energy_min_index]
 
+print(alpha)
+print(beta)
 print('                            energy_min [a.u]:  alpha:     alpha_woJ:   beta:   ')
-print('Method_(non-)interacting:           {:.4f}        {:.3f}   {:.5f}   {:.6f}     '.format(energy_min, alpha, alpha_woJ, beta ))
+print('Method_(non-)interacting:           {:.4f}        {:.3f}   {:.5f}   {:.6f}     '.format(float(energy_min), float(alpha), float(alpha_woJ), float(beta) ))
 
 
 
